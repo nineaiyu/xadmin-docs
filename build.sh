5a3d17301a5e3ea7c757a45a376ddef536dabf80
isummer <nineven@qq.com>
2024-10-25 14:56:38 +0800

    perf: 优化构建脚本

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #
 #
-# clean old build docker and image
-docker rm -f xadmin-docs-build
-docker image rm xadmin-docs-build
 
 # build web
 docker compose up xadmin-docs-build
+# clean old build docker
+docker rm -f xadmin-docs-build
