2ad5215e088f9ee329b4b0800649471a99aff154
isummer <nineven@qq.com>
2024-11-06 15:04:40 +0800

    chore: 升级依赖

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-#
-#
+
+DOCKER_IMAGE_PREFIX="swr.cn-north-4.myhuaweicloud.com/nineaiyu"
+
+images="xadmin-node:22.11.0-slim"
+for image in ${images};do
+  if ! docker images --format "{{.Repository}}:{{.Tag}}" |grep "^${image}" &>/dev/null;then
+    full_image_path="${DOCKER_IMAGE_PREFIX}/${image}"
+    docker pull  "${full_image_path}"
+    docker tag "${full_image_path}" "${image}"
+    docker rmi -f "${full_image_path}"
+  fi
+done
+
+
 
 # build web
 docker compose up xadmin-docs-build
