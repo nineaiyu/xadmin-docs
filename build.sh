12440d49dbace924f0951907bff23e25dc86d428
isummer <nineven@qq.com>
2024-11-22 20:06:12 +0800

    perf: 优化构建脚本

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+#
+
+VERSION=$1
 
 node_images="registry.cn-beijing.aliyuncs.com/nineaiyu/node:22.11.0-slim"
 
 npm_mirror="https://registry.npmmirror.com"
+
+
+
+if [[ -n ${VERSION} ]]; then
+sed -i "s@VERSION=.*@VERSION=v${VERSION/v/}@" guide/demo.md
+fi
+
 
 cmd='corepack enable && corepack prepare pnpm@9.12.3 --activate \
     && cd /app && pnpm install --frozen-lockfile && pnpm run docs:build'
